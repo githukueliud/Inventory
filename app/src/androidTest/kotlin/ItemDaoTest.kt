63e79a6a5ca00835279d14565ca14c342e6936bb
dev_eliud <githukueliud@gmail.com>
2023-09-04 19:23:46 -0500

    Delete and sell functionality done

--- app/src/androidTest/kotlin/ItemDaoTest.kt
+++ app/src/androidTest/kotlin/ItemDaoTest.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -71,5 +72,24 @@ class ItemDaoTest {
         assertEquals(allItems[1], item2)
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun daoUpdatesItems_UpdatesItemsInDb() = runBlocking{
+        addTwoItemsToDb()
+        itemDao.update(Item(1, "Apples", 15.0, 25))
+        itemDao.update(Item(2, "Bananas", 5.0, 50))
+        val allItems = itemDao.getAllItems().first()
+        assertEquals(allItems[0], Item(1, "Apples", 15.0, 25))
+        assertEquals(allItems[1], Item(2, "Bananas", 5.0, 50))
+    }
 
+    @Test
+    @Throws(Exception::class)
+    fun daoDeletesItems_DeleteAllItemsFromDb() = runBlocking{
+        addTwoItemsToDb()
+        itemDao.delete(item1)
+        itemDao.delete(item2)
+        val allItems = itemDao.getAllItems().first()
+        assertTrue(allItems.isEmpty())
+    }
 }
